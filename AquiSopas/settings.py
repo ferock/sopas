a1cf6876b8b9406d25eff61bd5a45a4210e1abd9
Fernando Jose Carbajal Martinez <ferock07@gmail.com>
2017-12-06 19:26:13 -0600

    prueba 2

--- AquiSopas/settings.py
+++ AquiSopas/settings.py
@@ -126,7 +126,7 @@ if not os.getenv('HAS_DB'):
             'NAME': os.getenv('DATABASE_NAME','appdb'),
             'USER': os.getenv('DATABASE_USER','default'),
             'PASSWORD': os.getenv('DATABASE_PASSWD','password'),
-            'HOST': os.getenv('MYSQL_SERVICE_HOST','localhost'),
+            'HOST': os.getenv('MYSQL_SERVICE_HOST','127.0.0.1'),
             'PORT': os.getenv('MYSQL_SERVICE_PORT','3306')
         },
         'TEST': {
